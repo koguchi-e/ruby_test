85708c63685ad07cb722f61b6cd813124e654133
ErikaK <koguchi.e915@gmail.com>
2025-01-19 21:10:27 +0900

    キャラクターファイルのコメントを全削除。
    Co-authored-by: Yuki Horikoshi <yuki-snow1823@users.noreply.github.com>

--- character.rb
+++ character.rb
@@ -1,23 +1,15 @@
-# require_relative：他のRubyファイルを読み込むためのメソッド
 require_relative './skills'
 
 class Character
   include Skills
-  # attr_accessor：「getter」と「setter」を自動的に生成するメソッド
   attr_accessor :level, :name, :hp
 
-  # initialize：クラスが新しく生成されるたびに初期化されるメソッド。
   def initialize(level: 5, name:, hp:)
-    # Rubyのインスタンス変数
-    # オブジェクトごとにデータを保持するためにインスタンス変数を使用
     @level = level
     @name = name
     @hp = hp
   end
 
-  ###########################################
-
-  # 防御の計算メソッド
   def calculate_damage(attack_power)
     defense_value = defend
     damage = [attack_power - defense_value, 0].max
@@ -25,7 +17,6 @@ class Character
     damage
   end
 
-  # ダメージ受けるメソッド
   def take_damage(value)
     @hp -= value
     puts "#{name} は #{value} のダメージを受けた！残りHP: #{@hp}"
